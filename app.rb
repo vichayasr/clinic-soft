8a6662cdcc7141916c981a6ed2c8d74987308570
Your Name <you@example.com>
2025-08-14 22:25:31 +0700

    feat(dev/node): add index route and page; rename demo-index.erb to demo.erb; add /demo route and move assets setup

--- app.rb
+++ app.rb
@@ -6,15 +6,20 @@ set :views, File.join(__dir__, "views")
 
 get "/" do
   @page_title = "Home"
-  @get_styles = ["/css/demo.css"]
-  @get_scripts = ["/plugins/psp-responsive/scripts/debug-mode.js"]
-  erb :"demo-index"
+  erb :index
 end
 
-# Serve assets from the template-level plugins directory at /plugins/*
-get "/plugins/*" do
+get "/demo" do
+  @page_title = "Demo"
+  @get_styles = ["/css/demo.css", "/npm/@psp/debug-mode/styles/debug-mode.css"]
+  @get_scripts = ["/npm/@psp/debug-mode/scripts/debug-mode.js"]
+  erb :demo
+end
+
+# Serve selected node_modules resources under /npm/*
+get "/npm/@psp/debug-mode/*" do
   relative = params["splat"].first
-  base_dir = File.expand_path(File.join(__dir__, "plugins"))
+  base_dir = File.expand_path(File.join(__dir__, "node_modules", "@psp", "debug-mode"))
   path = File.expand_path(File.join(base_dir, relative))
   halt 404 unless path.start_with?(base_dir) && File.file?(path)
   send_file path
