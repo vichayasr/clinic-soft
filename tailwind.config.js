c73f7e1a89b7e2905be1c41a12c8c0599294c331
Your Name <you@example.com>
2025-08-15 09:24:18 +0700

    Fix Tailwind v4 configuration and update theme plugin settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   content: ["./views/**/*.erb", "./public/**/*.html", "./app.rb"],
-  daisyui: {
-    themes: ['light', 'dark', 'cupcake'],
-  },
- plugins: [require("daisyui")],
 };
 
 
